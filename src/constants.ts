a8d71641d0753eb2a95459c20f5fa21c22cd1f30
Serhii Perehonchuk <inperegelion@gmail.com>
2023-01-23 14:33:05 +0200

    added build

--- src/constants.ts
+++
@@ -1,9 +0,0 @@
-export const ROUTE_ROOT = '/';
-
-export const ROUTE_GAME = '/game/';
-export const QUESTION_INDEX = 'questionIndex';
-export const ROUTE_GAME_TEMPLATE = `${ROUTE_GAME}:${QUESTION_INDEX}`;
-
-export const ROUTE_OVER = '/over/';
-export const YOUR_REMUNERATION = 'yourRemuneration';
-export const ROUTE_OVER_TEMPLATE = `${ROUTE_OVER}:${YOUR_REMUNERATION}`;
